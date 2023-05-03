2225c881e823d3b580d82dc6f3ad3c5ab9b58df0
mookbin <beeen1@naver.com>
2023-05-03 23:30:26 +0900

    lvfour

---
+++ lvfour/src/redux/config/configStore.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todos from "../modules/todosSlice";
+import comments from "../modules/commentsSlice";
+import comment from "../modules/commentSlice";
+import todo from "../modules/todoSlice";
+// import { isDev } from "../modules";
+
+const store = configureStore({
+  reducer: {
+    todos,
+    comments,
+    comment,
+    todo,
+  },
+});
+
+export default store;
